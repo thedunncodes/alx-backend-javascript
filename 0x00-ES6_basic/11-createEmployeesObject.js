9545270249de779b99c3aafa8fbfef7cba504acc
thedunncodes <tobioluwa348@gmail.com>
2024-03-09 18:03:40 +0000

    create objects

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-	const object = {
-		[departmentName]: employees
-	};
-	return object;
+  const object = {
+    [departmentName]: employees
+  }
+
+  return object;
 }
