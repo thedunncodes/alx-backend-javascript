512cb316818179894181222293fdb1cbfdae5315
thedunncodes <tobioluwa348@gmail.com>
2024-03-09 17:46:07 +0000

    loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let num = 0
-    for (const ar of array) {
-        array[num] = appendString + ar;
-        num += 1;
-    }
-  
-    return array;
+  const ar = [];
+  for (const val of array) {
+    ar.push(appendString + val);
+  }
+
+  return ar;
 }
