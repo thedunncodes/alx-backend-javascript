a75440ac0677ce0bc2d085d599304eacdb939e85
thedunncodes <tobioluwa348@gmail.com>
2024-03-09 19:24:41 +0000

    object method

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
     return {
-        allEmployees: employeesList,
+        allEmployees: { ...employeesList },
         getNumberOfDepartments(list) {
             return Object.keys(list).length
         },
