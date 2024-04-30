f95dec945621350f0c14c3149614d9defb27d274
thedunncodes <tobioluwa348@gmail.com>
2024-04-30 17:37:47 +0000

    task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
-function displayMessage(message) {
-    console.log(message);
-  };
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
-module.exports = displayMessage
+module.exports = displayMessage;
